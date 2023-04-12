22cf7d6a73572f486a9b1f0f2961ee7c69662df0
Curly-stasya <91792338+Curly-stasya@users.noreply.github.com>
2023-04-13 01:17:08 +0300

    Add files via upload

---
+++ lab5/StringException.java
@@ -0,0 +1,8 @@
+public class StringException extends RuntimeException {
+    public StringException(){
+        super();
+    }
+    public StringException(String message){
+        super(message);
+    }
+}
